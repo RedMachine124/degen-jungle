cc4873b40f80dbfb594f5517ceb74827f5bc71fd
RedMachine124 <45766454+RedMachine124@users.noreply.github.com>
2025-11-03 21:27:24 -0600

    Create tokens.ts

---
+++ src/config/tokens.ts
@@ -0,0 +1,30 @@
+export const TOKENS = [
+  {
+    symbol: 'SOL',
+    name: 'Solana',
+    mint: 'So11111111111111111111111111111111111111112',
+    pyth: 'Crypto.SOL/USD',
+    logo: 'https://assets.coingecko.com/coins/images/4128/large/solana.png'
+  },
+  {
+    symbol: 'USDC',
+    name: 'USD Coin',
+    mint: 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v',
+    pyth: 'Crypto.USDC/USD',
+    logo: 'https://assets.coingecko.com/coins/images/6319/large/USD_Coin_icon.png'
+  },
+  {
+    symbol: 'BONK',
+    name: 'Bonk',
+    mint: 'DezXzRpDQY8yS1t8AGoa5YG1cVjwbw5CEjoxdFyts3AQ',
+    pyth: 'Crypto.BONK/USD',
+    logo: 'https://assets.coingecko.com/coins/images/28600/large/bonk.jpg'
+  },
+  {
+    symbol: 'WIF',
+    name: 'dogwifhat',
+    mint: '4k3Dyjzvzp8eM7xyz7KQnGZq7rdt88v84oco7yZ67P4T',
+    pyth: null, // no Pyth yet
+    logo: 'https://assets.coingecko.com/coins/images/33875/large/wif.png'
+  }
+];
